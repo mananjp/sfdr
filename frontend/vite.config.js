05d70a5e214b5ceca87217a8a702c5ea201d2822
Tirth Patel <tbhanderi872@rku.ac.in>
2026-06-11 13:43:01 +0530

    added suport to deploy on render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://sfdr-backend.onrender.com',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
     },
